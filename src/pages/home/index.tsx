4111479cefd2ddb75b38d57b33f30fc2508e62ea
Matheus Silva <ms25022003@gmail.com>
2022-05-16 16:38:46 -0300

    configuring TS files

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
-import Header from '../../components/header';
+import Header from "../../components/header";
 import api from "../../service/api";
-import './index.css'
+
+export interface Film {
+    adult:             boolean;
+    backdrop_path:     string;
+    genre_ids:         number[];
+    id:                number;
+    original_language: string;
+    original_title:    string;
+    overview:          string;
+    popularity:        number;
+    poster_path:       string;
+    release_date:      Date;
+    title:             string;
+    video:             boolean;
+    vote_average:      number;
+    vote_count:        number;
+}
 
 function App() {
-    const [resultFilms, setResultFilms] = useState([]);
-    const [film, setFilm] = useState("");
+    const [resultFilms, setResultFilms] = useState<Film[]>([]);
+    const [film, setFilm] = useState<string>("");
 
-    const getNameFilm = (e) => {
-        setFilm(e.target.value);
-    }
 
 
     const loadDados = async () => {
-        if (film != "") {
+        if (film) {
             try {
                 var response = await api.get("/3/search/movie", { params: { query: `${film}` } })
                 console.log(response)
@@ -29,12 +42,11 @@ function App() {
             <Header />
             <h1>Seu site favorito para busca de filmes</h1>
             <div>
-                <input type="text" onChange={getNameFilm} placeholder="Nome do filme" />
+                <input type="text" onChange={(e) => setFilm(e.target.value)} placeholder="Nome do filme" />
                 <button onClick={loadDados} >Buscar</button>
             </div>
             {/* get imagem de filme usar https://image.tmdb.org/t/p/w500/ */}
             {resultFilms.map(e => {
-                console.log(e.poster_path)
                 return (
                     <div key={e.id}>
                         <img src={`https://image.tmdb.org/t/p/w500${e.poster_path}`} alt={e.title} />
