fb0694f2befe8c326091340617a6e6043317594c
Matheus Silva <ms25022003@gmail.com>
2022-06-08 11:29:38 -0300

    Carousel 2.0

--- src/components/CardDestaques.tsx
+++ src/components/CardDestaques.tsx
@@ -1,75 +1,24 @@
-import { ChevronLeftIcon, ChevronRightIcon, CurrencyBangladeshiIcon } from '@heroicons/react/solid';
-import React, { ButtonHTMLAttributes, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { IFilm } from '../interfaces/Film';
 import api from '../service/api';
-import CardFilm from './CardFilm';
+import Carousel from './Carousel';
 
 const CardDestaques = () => {
-  const [filmsDestaques, setFilmDestaques] = useState<IFilm[][]>();
+  const [filmsDestaques, setFilmDestaques] = useState<IFilm[]>([]);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     api.get('/discover/movie?sort_by=popularity.desc').then((response) => {
-      separar(response.data.results, 3)
+      setFilmDestaques(response.data.results)
     }).catch(err => {
       console.log(err)
     })
   }, [])
 
-  const separar = (base: [], maximo: number) => {
-    var resultado: IFilm[][] = [[]];
-    var grupo = 0;
-
-    for (var indice = 0; indice < base.length; indice++) {
-      if (resultado[grupo] === undefined) {
-        resultado[grupo] = [];
-      }
-      resultado[grupo].push(base[indice]);
-
-      if ((indice + 1) % maximo === 0) {
-        grupo = grupo + 1;
-      }
-    }
-    setFilmDestaques(resultado)
-  }
-  
-  
   return (
     <>
       <h1 className='text-[32px] font-light pt-16 px-9'>Em destaques</h1>
-      <div className='relative w-100'>
-        <div className='w-full flex items-center snap-mandatory snap-x overflow-x-auto scroll' onScroll={(e) => console.log(e.target)} ref={ref}>
-          {filmsDestaques?.map((film, index) => (
-            <div key={index} className="w-full flex-none flex snap-center justify-evenly mb-1">
-              {film.map((film) => (
-                <CardFilm key={film.id} film={film} />
-              ))}
-            </div>
-          )
-          )}
-          <button
-            onClick={() => {
-              ref.current?.scrollBy({
-                left: 1,
-                behavior: 'smooth'
-              })
-            }}
-            className='absolute right-2 top-1/2 disabled:bg-red'>
-            <ChevronRightIcon className='h-10 w-10' />
-          </button>
-          <button
-            onClick={() => {
-              ref.current?.scrollBy({
-                left: -1,
-                behavior: 'smooth'
-              })
-            }}
-            disabled={ref.current?.scrollLeft === 0}
-            className='absolute left-2 top-1/2 disabled:bg-red-600'>
-            <ChevronLeftIcon className='h-10 w-10' />
-          </button>
-        </div>
-      </div>
+      <Carousel items={filmsDestaques} visibleItemsNumber={3}/>
     </>
   )
 }
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,57 @@
+import ChevronLeftIcon from "@heroicons/react/solid/ChevronLeftIcon";
+import ChevronRightIcon from "@heroicons/react/solid/ChevronRightIcon";
+import { useState } from "react";
+import { IFilm } from "../interfaces/Film";
+import CardFilm from "./CardFilm";
+
+interface CarouselProps {
+    items: IFilm[];
+    visibleItemsNumber: number;
+}
+
+
+const Carousel = ({ items, visibleItemsNumber }: CarouselProps) => {
+
+    const [start, setStart] = useState(0);
+
+    const isControlsVisible = items?.length > visibleItemsNumber;
+
+    const visibleItems = isControlsVisible
+        ? items
+            .concat(items.slice(0, visibleItemsNumber))
+            .slice(start, start + visibleItemsNumber)
+        : items;
+
+    const onNextClick = () => {
+        setStart(start + visibleItemsNumber >= items.length ? items.length : start + visibleItemsNumber);
+    };
+
+    const onPrevClick = () => {
+        setStart(start - visibleItemsNumber >= 0 ? start - visibleItemsNumber : 0);
+    };
+
+    return (
+        <>
+            <div className='relative w-100'>
+                <div className='w-full flex items-center justify-evenly'>
+                    {visibleItems.map((item, index) => (
+                        <CardFilm key={index} film={item} />
+                    ))}
+                </div>
+                <button
+                    onClick={() => onPrevClick()}
+                    disabled={start === 0}
+                    className='absolute left-2 top-1/2 disabled:opacity-20 duration-150'>
+                    <ChevronLeftIcon className='h-10 w-10' />
+                </button>
+                <button
+                    disabled={start + visibleItemsNumber >= items.length}
+                    onClick={() => onNextClick()}
+                    className='absolute right-2 top-1/2 disabled:opacity-20 duration-150'>
+                    <ChevronRightIcon className='h-10 w-10' />
+                </button>
+            </div>
+        </>
+    )
+}
+export default Carousel;
