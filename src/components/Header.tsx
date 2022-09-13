d31e201a5c7dcdb8671b9dfddc163beb40e74c61
Matheus Silva <ms25022003@gmail.com>
2022-09-13 16:05:38 -0300

    header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,47 @@
-import React, { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Bars3Icon, ChevronDoubleDownIcon, XMarkIcon } from '@heroicons/react/20/solid';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/icons/logo.svg';
-import { ChevronDoubleDownIcon } from '@heroicons/react/solid'
-import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [query, setQuery] = useState("");
-    const inputRef = useRef<HTMLInputElement>(null);
+    const [isNavOpen, setIsNavOpen] = useState(false);
     const navigate = useNavigate();
 
     return (
-        <header className='bg-black shadow-[0_1px_5px] shadow-slate-700 text-white'>
-            <nav className='w-full flex justify-between p-[19px]'>
-                <div className='logo'><Link to="/"><img src={logo} alt="" /></Link></div>
-                <ul className='flex items-center text-2xl font-light'>
+        <header className='bg-black shadow-[0_1px_5px] shadow-slate-700 text-white sticky top-0 z-40 lg:relative'>
+            <nav className='w-full flex items-center justify-between px-3 py-5 lg:p-[19px]'>
+                <div className='logo w-44 lg:w-full'><Link to="/"><img src={logo} alt="logo site" /></Link></div>
+                <ul className='DESKTOP-MENU items-center text-2xl gap-6 font-light hidden lg:flex'>
                     <li>
-                        <div className="bg-gradient-to-b to-transparent w-full flex items-center flex-col">
-                            <form action="submit"
-                                onSubmit={(e) => {
-                                    e.preventDefault()
-                                    navigate(`film/${query}`)
-                                }}>
-                                <p onClick={() => inputRef.current?.focus()}>Lupa</p>
+                        <form action="submit"
+                            onSubmit={(e) => {
+                                e.preventDefault()
+                                navigate(`film/${query}`)
+                            }}>
+                            <label htmlFor="input" className='bg-search-icon bg-no-repeat bg-bottom cursor-pointer'>
                                 <input
-                                    className="max-w-[250px] py-1 px-9 text-[16px] font-light bg-black-bright text-white rounded-sm font-merri outline-none border-1 border-transparent hidden focus:border-white focus:flex"
+                                    id='input'
+                                    name='input'
+                                    className="max-w-[250px] py-1 text-[16px] font-light w-10 opacity-0 bg-black-bright text-white rounded-sm font-merri outline-none border-1 border-transparent focus:border-white focus:w-auto focus:px-9 focus:opacity-100 bg-search-icon bg-no-repeat bg-[center_left_5px]"
                                     type="text"
                                     onChange={(e) => setQuery(e.target.value)}
-                                    ref={inputRef}
                                     placeholder="Busca..." />
-                            </form>
-                        </div>
+                            </label>
+                        </form>
                     </li>
                     <li className='cursor-pointer hover:underline'
                         onClick={() => navigate('/')}>Home</li>
-                    <li className='cursor-pointer ml-6 flex items-center hover:underline'>Busca recentes <ChevronDoubleDownIcon className='ml-1' width={24} height={24} /></li>
+                    <li className='cursor-pointer flex items-center hover:underline'>Busca recentes <ChevronDoubleDownIcon className='ml-1' width={24} height={24} /></li>
                 </ul>
+                <section className='MOBILE-MENU flex lg:hidden'>
+                    <div className='' >
+                        {isNavOpen ? <XMarkIcon className='w-8' onClick={() => setIsNavOpen(false)} /> : <Bars3Icon className='w-8' onClick={() => setIsNavOpen(prev => !prev)}/> }
+                    </div>
+                    <div className={isNavOpen ? 'block absolute top-16 right-0 bg-black w-full h-screen z-50' : 'hidden'}>
+                        sdnaklsndlksa
+                    </div>
+                </section>
             </nav>
         </header>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      backgroundImage: {
+        'search-icon': "url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjEiIGhlaWdodD0iMjAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxkZWZzPjxjaXJjbGUgaWQ9ImEiIGN4PSI4IiBjeT0iOCIgcj0iOCIvPjxtYXNrIGlkPSJiIiBtYXNrQ29udGVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgbWFza1VuaXRzPSJvYmplY3RCb3VuZGluZ0JveCIgeD0iMCIgeT0iMCIgd2lkdGg9IjE2IiBoZWlnaHQ9IjE2IiBmaWxsPSIjZmZmIj48dXNlIHhsaW5rOmhyZWY9IiNhIi8+PC9tYXNrPjwvZGVmcz48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIG9wYWNpdHk9Ii43Ij48dXNlIHN0cm9rZT0iI0ZGRiIgbWFzaz0idXJsKCNiKSIgc3Ryb2tlLXdpZHRoPSI0IiB4bGluazpocmVmPSIjYSIvPjxwYXRoIGQ9Im0xMy40MTQgMTEgNi4zNjcgNi4zNjdhLjk5NS45OTUgMCAwIDEtLjAwMyAxLjQxMWMtLjM5LjM5LTEuMDMuMzg1LTEuNDEuMDAzTDEyIDEyLjQxNCAxMy40MTQgMTFaIiBmaWxsPSIjRkZGIi8+PC9nPjwvc3ZnPg==')"
+      },
       backgroundColor: {
         "black": {
           DEFAULT: "#232222",
